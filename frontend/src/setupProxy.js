d744f4a275a186fefae6a2b13d1b5a48b33ffa5e
Michi Devall <85964463+michidevall@users.noreply.github.com>
2023-09-07 18:20:07 -0600

    proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,5 +1,6 @@
-module.exports = function(app) {
-    app.use(
-      createProxyMiddleware(["/api", , "/otherApi"], { target: "https://backendport-31dbfb5a9023.herokuapp.com/" })
-    );
-  };
+const { createProxyMiddleware } = require('http-proxy-middleware');
+     
+    module.exports = function(app) {
+        app.use(createProxyMiddleware('/api/**', { target: 'https://backendport-31dbfb5a9023.herokuapp.com/' }));
+        app.use(createProxyMiddleware('/otherApi/**', { target: 'http://127.0.0.1:8000' }));
+    };
